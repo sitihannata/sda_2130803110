839cd93c71aa8b2c143fe066b74d02106da37f7e
sitihannata <113479598+sitihannata@users.noreply.github.com>
2022-10-25 20:50:19 +0700

    Add files via upload

---
+++ latihanArray.java
@@ -0,0 +1,8 @@
+public class latihanArray{
+    public static void main(String[] args){
+        int[] ages= new int [10];
+        for(int i=0;i<ages.length;i++){
+            System.out.print(ages[i]);
+        }
+    }
+}
